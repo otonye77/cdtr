e41206134f3dad33ef817f54dc86a7d88697b1f8
otonye77 <oamietubodie@gmail.com>
2023-04-24 21:03:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+function CountdownTimer() {
+  const [mins, setMins] = useState(0);
+  const [secs, setSecs] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    let intervalId;
+
+    if (isActive && (mins > 0 || secs > 0)) {
+      intervalId = setInterval(() => {
+        if (secs === 0) {
+          setMins(mins - 1);
+          setSecs(59);
+        } else {
+          setSecs(secs - 1);
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(intervalId);
+  }, [isActive, mins, secs]);
+
+  const handleStart = () => setIsActive(true);
+  const handleReset = () => {
+    setIsActive(false);
+    setMins(0);
+    setSecs(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Countdown Timer</h2>
+      <div>
+        <label>Minutes:</label>
+        <input type="number" onChange={(e) => setMins(parseInt(e.target.value))} />
+      </div>
+      <div>
+        <label>Seconds:</label>
+        <input type="number" onChange={(e) => setSecs(parseInt(e.target.value))} />
+      </div>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleReset}>Reset</button>
+      <div>{`${mins.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`}</div>
     </div>
   );
 }
 
-export default App;
+export default CountdownTimer;
